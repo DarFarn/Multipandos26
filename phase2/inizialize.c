1686c1b84aa07404f6f1cfcdb4016649a085731d
DarFarn <167864548+DarFarn@users.noreply.github.com>
2026-03-25 18:15:14 +0100

    Update inizialize.c

--- phase2/inizialize.c
+++ phase2/inizialize.c
@@ -5,13 +5,23 @@
 #include "../phase1/headers/pcb.h"
 #include <uriscv/liburiscv.h>
 #include <uriscv/types.h>
+#include <uriscv/const.h>
+
 
 
 int processCount; 			//numero di processi iniziati ma non terminati 
 int softblockcount;			//numero di processi bloccati
 LIST_HEAD(readyQueue);      
-pcb_t* current_process;    //puntatore al processo corrente
-semd_t device_semaphores; //QUANTI DEVO FARNE
+pcb_PTR current_process;    //puntatore al processo corrente
+semd_t device_semaphores;  //QUANTI DEVO FARNE?????????
+
+passupvector_t *pvector = (passupvector_t *)PASSUPVECTOR;
+
+pvector->tlb_refll_handler = (memaddr)uTLB_RefillHandler; //da rivedere 
+
+
+
+
 
 
 
