94846ec7e8407f51c4680a44beb5d77c2971156b
testaoncode <giuliotestoni04@gmail.com>
2026-03-26 19:15:53 +0100

    Update scheduler.c

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -1,4 +1,11 @@
-
+#include "../headers/listx.h"
+#include "../headers/const.h"
+#include "../headers/types.h"
+#include "../phase1/headers/asl.h"
+#include "../phase1/headers/pcb.h"
+#include <uriscv/liburiscv.h>
+#include <uriscv/types.h>
+#include <uriscv/const.h>
 //In its simplest form whenever the Scheduler is called it should dispatch the “next” process in the Ready Queue.
 //1. Remove the PCB from the head of the Ready Queue and store the pointer to the PCB in the
 //Current Process field.
@@ -10,5 +17,10 @@ LDIT(5)
 LDST(p_s);
 //Dispatching a process transitions it from a “ready” process to a “running” process
 //??
+//The Scheduler should behave in the following manner if the Ready Queue is empty:
 if (emptyProcQ(*readyQueue)){
+//1. If the Process Count is 0, invoke the HALT BIOS service/instruction [Section 13.2]. Consider this a job well done
+ HALT();}
+if(
+
 
