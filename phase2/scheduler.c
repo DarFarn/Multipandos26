19ff7a47bcadca67b5a0acab318ff995a2c1a2a8
testaoncode <giuliotestoni04@gmail.com>
2026-03-26 19:11:56 +0100

    Update scheduler.c

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -1,7 +1,14 @@
+
 //In its simplest form whenever the Scheduler is called it should dispatch the “next” process in the Ready Queue.
 //1. Remove the PCB from the head of the Ready Queue and store the pointer to the PCB in the
 //Current Process field.
+pcb t *outProcQ(struct list head *readyQueue, pcb t *p)
 //2. Load 5 milliseconds on the PLT [Section 7.2].
+LDIT(5)
 //3. Perform a Load Processor State (LDST) [Section 13.2] on the processor state stored in PCB of
 //the Current Process (p_s) of the current CPU.
+LDST(p_s);
 //Dispatching a process transitions it from a “ready” process to a “running” process
+//??
+if (emptyProcQ(*readyQueue)){
+
