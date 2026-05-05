1cb5887534201265cd625de39278b38762dc7ced
testaoncode <giuliotestoni04@gmail.com>
2026-05-05 15:51:33 +0200

    Update initProc.c

--- phase3/initProc.c
+++ phase3/initProc.c
@@ -1,41 +1,75 @@
-//implements test 
 
-//inizializzerà:
-#include "../headers/listx.h"
-#include "../headers/const.h"
-#include "../headers/types.h"
-#include "../phase1/headers/asl.h"
-#include "../phase1/headers/pcb.h"
-#include <uriscv/liburiscv.h>
-#include <uriscv/types.h>
-#include <uriscv/const.h>
-#include "../headers/klog.h"
-#include "../phase2/headers/exceptions.h"
-#include "../phase2/headers/scheduler.h"
-#include "../phase3/headers/initProc.h"
-
-//da implementare
-
-//1. test(), entry point deve inizializzare (non vuol dire definire qua!):
-//swap pool structure, per l'appunto definite in vmSupport 
-//swap mutex, capire dove sta
-//terminal semaphores, due dato che dovrebbe esserci un solo terminale alla fine 
-//mastersemaphore per la gestione dello stesso istantiator process
-//shellSemaphore 
-//creare processo shell (tramite NSYS apposito?)
-//poi alla fine eseguo P sul mastersemaphore e termino con NSYS2
-
+#include "../phase3/headers/support.h"
 
 support_t support_structs[UPROCMAX];
-void initUProc(int asid){
-//che inizializzerà, per lo specifico processo:
-//sup_asid
-//sup_privatePgTbl[32]
-//sup_exceptContext[2]
-SYSCALL(CREATEPROCESS, state, support_ptr, 0);
 
-//inizializzare pagetable, lasciare in h solo struttura e qua inizializzazione vera e propria
+int masterSem = 0;
+int shellSem = 0;
 
+/* declared in vmSupport.c */
+extern void initSwapStructs();
+
+/* forward */
+void initUProc(int asid);
+
+/* ===================== TEST ===================== */
+void test() {
+
+    /* init swap structures */
+    initSwapStructs();
+
+    /* init semaphores */
+    masterSem = 0;
+    shellSem = 0;
+
+    /* create shell (ASID = 1 for simplicity) */
+    initUProc(1);
+
+    /* wait for shell termination */
+    SYSCALL(PASSEREN, (int)&masterSem, 0, 0);
+
+    /* terminate */
+    SYSCALL(TERMINATE, 0, 0, 0);
+}
+
+/* ===================== INIT UPROC ===================== */
+void initUProc(int asid) {
+
+    support_t *sup = &support_structs[asid - 1];
+
+    sup->sup_asid = asid;
+
+    /* -------- PAGE TABLE INIT -------- */
+    for (int i = 0; i < 31; i++) {
+        sup->sup_privatePgTbl[i].entry_hi = (0x80000 + i) | (asid << ASIDSHIFT);
+        sup->sup_privatePgTbl[i].entry_lo = DIRTYON;
+    }
+
+    /* stack page */
+    sup->sup_privatePgTbl[31].entry_hi = (0xBFFFF) | (asid << ASIDSHIFT);
+    sup->sup_privatePgTbl[31].entry_lo = DIRTYON;
+
+    /* -------- EXCEPTION CONTEXT -------- */
+    sup->sup_exceptContext[PGFAULTEXCEPT].c_pc = (memaddr)pager;
+    sup->sup_exceptContext[PGFAULTEXCEPT].c_status = MSTATUS_MPP_M | MSTATUS_MPIE_MASK;
+    sup->sup_exceptContext[PGFAULTEXCEPT].c_stackPtr =
+        (memaddr)&sup->sup_stackTLB[499];
+
+    sup->sup_exceptContext[GENERALEXCEPT].c_pc = (memaddr)generalExceptionHandler;
+    sup->sup_exceptContext[GENERALEXCEPT].c_status = MSTATUS_MPP_M | MSTATUS_MPIE_MASK;
+    sup->sup_exceptContext[GENERALEXCEPT].c_stackPtr =
+        (memaddr)&sup->sup_stackGen[499];
+
+    /* -------- INITIAL STATE -------- */
+    state_t state;
+
+    state.s_pc = 0x800000B0;
+    state.s_sp = 0xC0000000;
+    state.s_status = MSTATUS_MPP_U | MSTATUS_MPIE_MASK;
+    state.s_entryHI = asid << ASIDSHIFT;
+
+    SYSCALL(CREATEPROCESS, (int)&state, (int)sup, 0);
+}
 
 
 
