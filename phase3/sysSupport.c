58b5513fa3a182dcc4b84ae304fdad079e51ca0b
testaoncode <giuliotestoni04@gmail.com>
2026-05-12 15:03:18 +0200

    Update sysSupport.c

--- phase3/sysSupport.c
+++ phase3/sysSupport.c
@@ -1,65 +1,58 @@
 #include "../headers/types.h"
-#include "../phase2/exceptions.c"
+#include <uriscv/types.h>
+#include <uriscv/liburiscv.h>
+#include "../headers/const.h"
+#include <uriscv/const.h>
+#include "../phase2/headers/exceptions.h"
+#include "../headers/support.h"
+
 //implements:
-void generalExceptionHandler() {
+
+
+void supsyscallHandler() {
 
     support_t *sup = (support_t *) SYSCALL(GETSUPPORTPTR, 0, 0, 0);
-
     state_t *state = &sup->sup_exceptState[GENERALEXCEPT];
 
-    if ((state->cause & EXCCODE_MASK) == SYSCALL)
-        syscallHandler();
-    else
-        programTrapHandler();
-}
-
-
-void syscallHandler() {
-
-    support_t *sup = (support_t *) SYSCALL(GETSUPPORTPTR, 0, 0, 0);
-
-    state_t *state = &sup->sup_exceptState[GENERALEXCEPT];
-
-    /* a0 = x10 */
     int sysno = state->gpr[10];
 
     switch (sysno) {
 
         case TERMINATE:
-
             if (sup->sup_asid == 1)
                 SYSCALL(VERHOGEN, (int)&masterSem, 0, 0);
             else
                 SYSCALL(VERHOGEN, (int)&shellSem, 0, 0);
 
             SYSCALL(TERMINATE, 0, 0, 0);
-
             break;
 
-
-        case EXECUTE:
-
-            /* a1 = x11 */
+        case CREATEPROCESS:
             initUProc(state->gpr[11]);
-
             SYSCALL(PASSEREN, (int)&shellSem, 0, 0);
-
             break;
-
 
         default:
-
             SYSCALL(TERMINATE, 0, 0, 0);
     }
 
-    /* advance PC */
     state->pc_epc += 4;
-
     LDST(state);
 }
 
 
-void programTrapHandler() {
-
+void supprogramTrapHandler() {
     SYSCALL(TERMINATE, 0, 0, 0);
 }
+void supgeneralExceptionHandler() {
+
+    support_t *sup = (support_t *) SYSCALL(GETSUPPORTPTR, 0, 0, 0);
+
+    state_t *state = &sup->sup_exceptState[GENERALEXCEPT];
+
+    if (((getCAUSE() & GETEXECCODE) >> 2) == SYSCALL)
+        supsyscallHandler();
+    else
+        supprogramTrapHandler();
+}
+
