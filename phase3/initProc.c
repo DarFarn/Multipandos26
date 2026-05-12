f3617ca84a4e7d35aa37e6bbb290df59845fe93d
testaoncode <giuliotestoni04@gmail.com>
2026-05-12 15:04:11 +0200

    Update initProc.c

--- phase3/initProc.c
+++ phase3/initProc.c
@@ -1,16 +1,16 @@
+#include "../headers/types.h"
+#include <uriscv/liburiscv.h>
+#include "../headers/const.h"
+#include <uriscv/types.h>
+#include "../headers/const.h"
+#include <uriscv/const.h>
+#include "../phase2/headers/exceptions.h"
+#include "../headers/support.h"
 
-#include "../phase3/headers/support.h"
-
-support_t support_structs[UPROCMAX];
+support_t support_structs[UPROCMAX]; //crea un array di 8 support_t, già definita in types.h
 
 int masterSem = 0;
-int shellSem = 0;
-
-/* declared in vmSupport.c */
-extern void initSwapStructs();
-
-/* forward */
-void initUProc(int asid);
+int shellSem= 0;
 
 /* ===================== TEST ===================== */
 void test() {
@@ -26,7 +26,7 @@ void test() {
     initUProc(1);
 
     /* wait for shell termination */
-    SYSCALL(PASSEREN, (int)&masterSem, 0, 0);
+    SYSCALL(PASSEREN, (int)&masterSem, 0, 0); //dove è definita PASSEREN così in maiuscolo?
 
     /* terminate */
     SYSCALL(TERMINATE, 0, 0, 0);
@@ -41,36 +41,35 @@ void initUProc(int asid) {
 
     /* -------- PAGE TABLE INIT -------- */
     for (int i = 0; i < 31; i++) {
-        sup->sup_privatePgTbl[i].entry_hi = (0x80000 + i) | (asid << ASIDSHIFT);
-        sup->sup_privatePgTbl[i].entry_lo = DIRTYON;
+        sup->sup_privatePgTbl[i].pte_entryHI = (0x80000 + i) | (asid << ASIDSHIFT);
+        sup->sup_privatePgTbl[i].pte_entryLO = DIRTYON;
     }
 
     /* stack page */
-    sup->sup_privatePgTbl[31].entry_hi = (0xBFFFF) | (asid << ASIDSHIFT);
-    sup->sup_privatePgTbl[31].entry_lo = DIRTYON;
+    sup->sup_privatePgTbl[31].pte_entryHI = (0xBFFFF) | (asid << ASIDSHIFT);
+    sup->sup_privatePgTbl[31].pte_entryLO = DIRTYON;
 
     /* -------- EXCEPTION CONTEXT -------- */
-    sup->sup_exceptContext[PGFAULTEXCEPT].c_pc = (memaddr)pager;
-    sup->sup_exceptContext[PGFAULTEXCEPT].c_status = MSTATUS_MPP_M | MSTATUS_MPIE_MASK;
-    sup->sup_exceptContext[PGFAULTEXCEPT].c_stackPtr =
+    sup->sup_exceptContext[PGFAULTEXCEPT].pc = (memaddr)pager;
+    sup->sup_exceptContext[PGFAULTEXCEPT].status = MSTATUS_MPP_M | MSTATUS_MPIE_MASK;
+    sup->sup_exceptContext[PGFAULTEXCEPT].stackPtr =
         (memaddr)&sup->sup_stackTLB[499];
 
-    sup->sup_exceptContext[GENERALEXCEPT].c_pc = (memaddr)generalExceptionHandler;
-    sup->sup_exceptContext[GENERALEXCEPT].c_status = MSTATUS_MPP_M | MSTATUS_MPIE_MASK;
-    sup->sup_exceptContext[GENERALEXCEPT].c_stackPtr =
+    sup->sup_exceptContext[GENERALEXCEPT].pc = (memaddr)supgeneralExceptionHandler;
+    sup->sup_exceptContext[GENERALEXCEPT].status = MSTATUS_MPP_M | MSTATUS_MPIE_MASK;
+    sup->sup_exceptContext[GENERALEXCEPT].stackPtr =
         (memaddr)&sup->sup_stackGen[499];
 
     /* -------- INITIAL STATE -------- */
     state_t state;
 
-    state.s_pc = 0x800000B0;
-    state.s_sp = 0xC0000000;
-    state.s_status = MSTATUS_MPP_U | MSTATUS_MPIE_MASK;
-    state.s_entryHI = asid << ASIDSHIFT;
+    state.pc_epc = 0x800000B0;
+    state.gpr[2] = 0xC0000000;
+    state.status = MSTATUS_MPP_U | MSTATUS_MPIE_MASK;
+    state.entry_hi = asid << ASIDSHIFT;
 
     SYSCALL(CREATEPROCESS, (int)&state, (int)sup, 0);
 }
-
 
 
 
